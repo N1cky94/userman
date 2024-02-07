f4efdf31db81648305d9365481379d6b088e7a9c
Nicky <nick.bauters@me.com>
2024-02-07 17:24:19 +0100

    #7: feat: Add user information to the database schema and data.sql files

--- data/data.sql
+++ data/data.sql
@@ -1,6 +1,8 @@
 insert into users
-(email,password,role)
+(email,password,role, account_activated)
 values
-    ('member@hoase.be','$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW','MEMBER'),
-    ('admin@hoase.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'ADMIN')
+    ('nick@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'ADMIN', true),
+    ('kelly@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', true),
+    ('fynn@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', false),
+    ('mitch@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', false)
 ;
