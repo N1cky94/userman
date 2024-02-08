c93afb38496339fd29002e6ef47ab44facd78028
Nicky <nick.bauters@me.com>
2024-02-08 15:17:10 +0100

    #12: feat: Create the fetchAllUsers use case and implement it

---
+++ src/main/java/be/archilios/usermanagement/core/users/UserDatabaseService.java
@@ -0,0 +1,20 @@
+package be.archilios.usermanagement.core.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDatabaseService implements UserUseCases {
+    private final UserRepository userRepository;
+    
+    public List<UserInfo> fetchAllUsers() {
+        return userRepository
+                .findAll()
+                .stream()
+                .map(UserInfo::from)
+                .toList();
+    }
+}
