4de095ca6755f478b9389786b6783722b85130da
Nicky <nick.bauters@me.com>
2024-02-08 11:34:46 +0100

    #7: feat: Create a Login page

---
+++ src/main/java/be/archilios/usermanagement/views/auth/LoginView.java
@@ -0,0 +1,48 @@
+package be.archilios.usermanagement.views.auth;
+
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("login")
+@PageTitle("Login")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+    private LoginForm login = new LoginForm();
+    
+    public LoginView() {
+        addClassName("login-view");
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setAlignItems(Alignment.CENTER);
+        
+        Image logo = new Image("images/public/Archilios-logo.jpg", "Archilios Logo");
+        logo.setMaxHeight(100, Unit.PIXELS);
+        logo.setMinHeight(20, Unit.PIXELS);
+        logo.setMaxWidth(80, Unit.PERCENTAGE);
+        
+        login.setAction("login");
+        add(
+                logo,
+                new H1("UseMan (User Manager)"),
+                login
+        );
+    }
+    
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        if (beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            login.setError(true);
+        }
+    }
+}
