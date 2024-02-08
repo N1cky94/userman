ef1698f0d7a9129b35ad717063696877ee49d6ca
Nicky <nick.bauters@me.com>
2024-02-08 15:04:52 +0100

    #12: feat: Create the User entity with updated database schema and dummy data for local use

--- data/data.sql
+++ data/data.sql
@@ -1,8 +1,8 @@
 insert into users
-(email,password,role, account_activated)
+(email,password,role, account_activated, first_name, last_name)
 values
-    ('nick@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'ADMIN', true),
-    ('kelly@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', true),
-    ('fynn@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', false),
-    ('mitch@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', false)
+    ('nick@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'ADMIN', true, 'Nick', 'Bauters'),
+    ('kelly@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', true, 'Kelly', 'Bauters'),
+    ('fynn@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', false, 'Fynn', 'Bauters'),
+    ('mitch@archilios.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'USER', false, 'Mitch', 'Bauters')
 ;
---
+++ src/main/java/be/archilios/usermanagement/core/users/User.java
@@ -0,0 +1,26 @@
+package be.archilios.usermanagement.core.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter(AccessLevel.PACKAGE)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "account_activated")
+    private boolean active;
+}
