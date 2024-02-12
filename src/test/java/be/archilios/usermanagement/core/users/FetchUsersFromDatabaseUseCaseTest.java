fee595915590503528be65c377a94ee03738b626
Nicky <nick.bauters@me.com>
2024-02-12 11:06:07 +0100

    #22: refactor: Rename DatabaseServiceTest class to describe the containing tests better

--- src/test/java/be/archilios/usermanagement/core/users/FetchUsersFromDatabaseUseCaseTest.java
+++ src/test/java/be/archilios/usermanagement/core/users/FetchUsersFromDatabaseUseCaseTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class UserDatabaseServiceTest {
+class FetchUsersFromDatabaseUseCaseTest {
     @Mock
     private UserRepository userRepository;
     
