c48d8c46369e75ee5a337c9b94c7ca36704d23ea
Nicky <nick.bauters@me.com>
2024-02-12 11:30:26 +0100

    #22: test: Create new User should save the information to the database service

--- src/test/java/be/archilios/usermanagement/core/users/CreateNewUserUseCaseTest.java
+++ src/test/java/be/archilios/usermanagement/core/users/CreateNewUserUseCaseTest.java
@@ -1,10 +1,15 @@
 package be.archilios.usermanagement.core.users;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateNewUserUseCaseTest {
@@ -19,7 +24,24 @@ public class CreateNewUserUseCaseTest {
     }
     
     @Test
+    @DisplayName("Given a new user, when the user is saved, then the user should be correctly created, saved to the repository and returned")
     void correctlyCreateNewUser() {
-    
+        when(userRepository.save(any()))
+                .thenReturn(
+                    UserMother.get()
+                            .asDeactivated()
+                            .build()
+                );
+        CreateNewUserCommand newUser = UserMother.get()
+                .asDeactivated()
+                .buildAsNewUserCommand();
+        UserInfo newUserInfo = UserMother.get()
+                .asDeactivated()
+                .buildAsUserInfo();
+        
+        UserInfo createdUser = userDatabaseService.createNewUser(newUser);
+        
+        assertNotNull(createdUser, "The created user should not be null");
+        assertEquals(newUserInfo, createdUser, "The created user should be correctly returned from the database");
     }
 }
