f98d0ffde9b5fc7a3394a0924df6af711b38669c
Nicky <nick.bauters@me.com>
2024-02-14 11:46:39 +0100

    demo01: 02: feat: Create the Security User nd UserDetails service

---
+++ src/main/java/be/archilios/usermanagement/security/user/UserDetailsFromSecurityUser.java
@@ -0,0 +1,51 @@
+package be.archilios.usermanagement.security.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class UserDetailsFromSecurityUser implements UserDetails {
+    private final SecurityUser user;
+    
+    public UserDetailsFromSecurityUser(SecurityUser securityUser) {
+        this.user = securityUser;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Set.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+    
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+    
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return user.isActiveAccount();
+    }
+}
