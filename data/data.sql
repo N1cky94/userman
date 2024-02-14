c430ce16c1f8a527a197e2699fdb58551fd3a587
Nicky <nick.bauters@me.com>
2024-02-14 10:22:05 +0100

    demo01: 01: feat: Add data and schema for test database

---
+++ data/data.sql
@@ -0,0 +1,6 @@
+insert into users
+(email,password,role)
+values
+    ('member@userman.be','$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW','MEMBER'),
+    ('admin@userman.be', '$2a$12$df4AxNKuBK9BPkXQ6iDNReoDOMWv/f8ALz5urhn87ZfSN.o0HzbRW', 'ADMIN')
+;
